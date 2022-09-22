570a5a4934e90fb258470f1b7250441d3bafd41a
vidabonita <chisomojo076@gmail.com>
2022-09-22 12:21:08 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -30,7 +30,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		num = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
-		}
+	}
 
 	for (; *n2; n2--, r_index--)
 	{
@@ -66,9 +66,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	for (index = 0; *(n1 + index); index++)
 		n1_len++;
-
-	for (index = 0; *(n2 + index); index++)
-		n2_len++;
 
 	for (index = 0; *(n2 + index); index++)
 		n2_len++;
