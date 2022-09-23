5776ea810bb24b7462a1e4b39fca5cd70fdf379f
vidabonita <chisomojo076@gmail.com>
2022-09-22 18:35:04 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,17 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: returns length as integer;
+ * _strlen - return the length of a string
+ *
+ * @s: char to check
+ *
+ * Description: this will return the length of a string
+ * Return: 0 is success
  */
-
 int _strlen(char *s)
 {
-	int len = 0;
-	
-	while (*(s + len) != '\0')
-		len++;
-	return (len);
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
 }
